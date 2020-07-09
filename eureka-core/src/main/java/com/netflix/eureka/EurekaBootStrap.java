f088763b898e0b414e3c065e65f588e861cbc92f
zhaojd-hub <zhaojd.hub@gmail.com>
2020-07-10 01:34:51 +0800

    fix

--- eureka-core/src/main/java/com/netflix/eureka/EurekaBootStrap.java
+++ eureka-core/src/main/java/com/netflix/eureka/EurekaBootStrap.java
@@ -189,7 +189,7 @@ public class EurekaBootStrap implements ServletContextListener {
             );
         }
 
-        // 第四步：处理peer节点相关的事情
+        // 第四步：处理peer节点相关的东西
         PeerEurekaNodes peerEurekaNodes = getPeerEurekaNodes(
                 registry,
                 eurekaServerConfig,
